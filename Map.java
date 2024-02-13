3a112bbc40a7cbd826be3560e52f7b7fa2bb598f
Jack Thompson <159587261+Jack-Thompson07@users.noreply.github.com>
2024-02-13 12:47:45 -0800

    Update Map.java

--- Map.java
+++ Map.java
@@ -1,8 +1,14 @@
 // Jack Thompson
 public class Map {
 
+    private int SIZE;
+    
+    private Cave[][] grid;
+    //private painter p;
+    
+    
     public Map(){
-
+        this.grid = new grid[SIZE][SIZE];
     }
 
     public void draw(){
