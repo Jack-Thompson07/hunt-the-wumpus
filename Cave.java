041a15b2d268d9daa2c42a69b4e50320650c9d7f
Jack Thompson <159587261+Jack-Thompson07@users.noreply.github.com>
2024-02-13 12:45:16 -0800

    Update Cave.java

--- Cave.java
+++ Cave.java
@@ -1,8 +1,23 @@
 // Jack Thompson
 public class Cave {
-    
-    public Cave(){
 
+    private Map map;
+    private Hazzard hazzard;
+
+    private int[] cords;
+    
+    public Cave(Map map, int x, int y, Hazzard h){
+        build();
+        this.hazzard = h;
+    }
+
+    public Cave(Map map, int x, int y){
+        build();
+    }
+
+    private class build(Map map, int x, int y){
+        this.map = map;
+        this.cords = new int[]{xCord, yCord}
     }
 
 }
