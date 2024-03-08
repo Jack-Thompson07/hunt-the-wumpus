f16e98c701ca904b2afe6ef4df7a62c5ee29ecd5
EthanWilde5 <1095713@apps.nsd.org>
2024-03-08 14:25:06 -0800

    Worked on comments for High score and changed Gui

--- Gui.java
+++ Gui.java
@@ -1,17 +1,26 @@
-public class Gui{
-    //////////////
-    //Properties
-    ///////////////
+// Graphical INterface object Displays the state of the 
+// current game (current room, connected trooms. Inventory. etc)
+// Class representing the pentagon. 
+// in charge of rendering everything
+// how am I gonna display the player in the cave and the cave itself
+// How to diplay the menu
+// Basically anything that the user can interact with I need to consider.
+
+public class GUI{
 
 
 
-    ///////////////
-    //Constructor
-    ///////////////
+//////////////
+// Properties
+//////////////
+
+//////////////
+// Constructors
+//////////////
 
 
 
-    ////////////////
-    //Methods
-    ////////////////
+//////////////
+// Methods
+//////////////
 }
