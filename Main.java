01d31bea98a91c324aaca59947e34a638ce2c96e
Jack-Thompson07 <1108850@apps.nsd.org>
2024-05-22 12:45:27 -0700

    4trtrtwr

--- Main.java
+++ Main.java
@@ -1,9 +1,10 @@
 public class Main{
 
-  static Gui gui = new Gui();
+  
   
   public static void main(String args[]){
     System.out.println("IT STARTS!");
+    Gui gui = new Gui();
     System.out.println("IT ENDS!");
   }
 
