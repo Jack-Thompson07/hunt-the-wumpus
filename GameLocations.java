e3eba24aa3a5df61a3506f9e33b082bd08f792dc
Jack Thompson <159587261+Jack-Thompson07@users.noreply.github.com>
2024-05-24 09:23:02 -0700

    Update GameLocations.java

--- GameLocations.java
+++ GameLocations.java
@@ -1,3 +1,4 @@
+
 // Jack Thompson
 
 import java.util.Scanner;
@@ -5,102 +6,29 @@ import java.io.File;
 
 public class GameLocations {
 
-    private final int HEIGHT = 5;
-    private final int WIDTH = 6;
 
     private Player player;
-    private Hazard hazard;
-
-    private Cave[][] grid;
     
-    Scanner reader;
-    String[] data;
+    private Cave cave;
 
-    private int[] totalHazards;
+    private String[][] hazards;
 
     // bats, caves
 
-    public GameLocations(Player p) {
-        this.grid = new Cave[HEIGHT][WIDTH];
-
-        this.player = p;
-        this.hazard = hazard;
-        readFile();
-        build();
+    public GameLocations() {
+        this.cave = new Cave();
+        this.player = new Player(new int[]{0,0});
     }
 
-    public void build() {
-
-        
-        this.grid = new Cave[HEIGHT][WIDTH];
-
-       for(int i = 0; i < HEIGHT; i ++){
-           for(int j = 0; j < WIDTH; j ++){
-               Cave current = this.grid[i][j] = new Cave((i * WIDTH + j + 1));
-               System.out.println((i * WIDTH + j + 1) + ") Room IS ADDED");
-    
-               for(String s : this.data[(i * WIDTH + j)].split(",")){
-                    current.addTunnel(Integer.parseInt(s));
-                    
-
-
-               }
-           }
-       }    
-       System.out.println("MAP IS BUILT");
+    public Cave getCave(){
+        return this.cave;
     }
 
-
-    public void setPlayerLocation(int[] cords) {
-
+    public String getHazardAt(int[] cords){
+        return this.hazards[cords[0]][cords[1]];
     }
 
-
-    public boolean checkWampusAlive() {
-        return true;
-    }
-
-    public boolean checkChestWithPlayer() {
-        return true;
-    }
-
-    public void addHazard(String hazard) {
-        if (hazard.equals("Bat"))
-            this.totalHazards[0]++;
-        else if (hazard.equals("Pit"))
-            this.totalHazards[1]++;
-    }
-
-    public Player getPlayer() {
+    public Player getPlayer(){
         return this.player;
     }
-
-    public Cave[][] getGrid(){
-        return this.grid;
-    }
-    
-    public Hazard getHazard(){
-        return this.hazard;
-    }
-
-    public void readFile(){
-        
-        File f = new File("StoredGrids.csv"); 
-        try {
-          this.reader = new Scanner(f);
-        }
-        catch(Exception e){
-            System.out.println("<No File Found>");
-        }
-        /*for(int i = 0; i < (int)(Math.random() * 5))
-            reader.nextLine();*/
-        
-        this.data = reader.nextLine().split(";");
-    }
-
-    public Cave getCave(int[] cords){
-        return this.grid[cords[0]][cords[1]];
-    }
-
-
 }
