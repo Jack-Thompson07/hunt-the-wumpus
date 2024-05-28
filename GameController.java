c563b31922df7b9b273b8c64051a4bbaa80fa964
Marcus Yao <1108147@apps.nsd.org>
2024-05-28 13:55:59 -0700

    sdwdw

--- GameController.java
+++ GameController.java
@@ -3,50 +3,39 @@ import java.util.Random;
 public class GameController {
 
     private GameLocations gl;
-    private GameLocations map;
-    private Player player;
-    private Scanner scanner;
-    private Hazard hazard;
-    private int coins;
-    private Random random;
+    private HighScore hs;
+    private Gui gui;
 
-
-    public GameController(){
-        this.map = new GameLocations(player);
-        gameBegin(scanner);
-        
+    public GameController() {
+        this.gl = new GameLocations();
+        this.gui = new Gui(this);
     }
 
-     public void gameBegin(Scanner scanner){
-      
-        
-      
+    // Called by GUI
+    // Returns if thePlayer is able to move to the given cords
+    // If the Player is able to move there, it will return true, and it will move
+    // the Player there.
 
-    }
-
-    
-
-    //Called by GUI
-    //Returns if the player is able to move to the given cords
-    //If the player is able to move there, it will return true, and it will move the player there. 
     public boolean movePlayer(int[] cords) {
 
         boolean validMove = false;
         int[] tunnels = this.gl.getCave().getTunnels(this.gl.getPlayer().getPosition());
 
         for (int i : tunnels) {
-            if (this.gl.getCave().getPosOfTunnel(this.gl.getPlayer().getPosition(), i) == cords)
+            if ((this.gl.getCave().getPosOfTunnel(this.gl.getPlayer().getPosition(), i)[0] == cords[0]) && (this.gl.getCave().getPosOfTunnel(this.gl.getPlayer().getPosition(), i)[1] == cords[1]))
                 validMove = true;
         }
 
         if (validMove) {
             this.gl.getPlayer().move(cords);
             this.gl.getPlayer().addMove();
+            System.out.println("Player moved");
+            this.gui.updateMap();
         }
 
         return validMove;
     }
-    
+
     public String checkHazard(){
      
         if(gl.getHazardAt(this.gl.getPlayer().getPosition()) == null){
@@ -59,11 +48,9 @@ public class GameController {
 
          return type;
      }
+}
 
-    public boolean checkWampusAlive() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'checkWampusAlive'");
+    public GameLocations getGameLocations(){
+        return this.gl;
     }
-
-     
 }
