525c304354e29cf18f151b9f8d7fb0785dfb66a5
EthanWilde5 <1095713@apps.nsd.org>
2024-05-29 12:28:50 -0700

    added Wumpus Location Method

--- GameLocations.java
+++ GameLocations.java
@@ -13,6 +13,8 @@ public class GameLocations {
 
     private String[][] hazards;
 
+    private String[][] wumpus;
+
     // bats, caves
 
     public GameLocations() {
@@ -69,5 +71,20 @@ public class GameLocations {
             col = (int)(Math.random() * 6);
         }
         this.player = new Player(new int[]{row,col});
+    }
+
+    public void WumpusPlace(){
+        this.wumpus = new String[5][6];
+        
+        for(int i = 0; i < 2; i ++){
+            int col = -1;
+            int row = -1;
+            while((row == -1) || (this.wumpus[row][col] != null)){
+                row = (int)(Math.random() * 5);
+                col = (int)(Math.random() * 6);
+            }
+            System.out.println(row + " - " + col);
+            this.wumpus[row][col] = "Wumpus";
+        }
     }
 }
